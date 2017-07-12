0c1ea77ed7bcea3734f88313de7f51723c1dc8f4
Ryan Bond <drbond@gmail.com>
2017-07-12 10:58:51 -0500

    added the textwatcher and change listener.

--- app/src/main/java/drbond/android_fortune_teller/MainActivity.java
+++ app/src/main/java/drbond/android_fortune_teller/MainActivity.java
@@ -2,6 +2,9 @@ package drbond.android_fortune_teller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
@@ -12,6 +15,9 @@ import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     private TextView answerText;
+    String userQuestion = "";
+    String[] fortunes = getResources().getStringArray(R.array.fortunes);
+
 
 
 
@@ -19,11 +25,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TextView References
         answerText = (TextView) findViewById(R.id.answerText);
 
-
-
+        // TextWatchers
+        EditText enterQuestion =
+                (EditText) findViewById(R.id.enterQuestion);
+        enterQuestion.addTextChangedListener(enterQuestionTextWatcher);
 
     }
+
+    // listener object for DownPayment EditText
+    private final TextWatcher enterQuestionTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            userQuestion = s.toString();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
 }
