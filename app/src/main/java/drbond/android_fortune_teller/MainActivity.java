006cb554358a5e8321f78cd31bdd36c9d515f387
Ryan Bond <drbond@gmail.com>
2017-07-12 10:31:42 -0500

    created the layout and supporting strings

--- app/src/main/java/drbond/android_fortune_teller/MainActivity.java
+++ app/src/main/java/drbond/android_fortune_teller/MainActivity.java
@@ -2,12 +2,28 @@ package drbond.android_fortune_teller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import java.util.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView answerText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerText = (TextView) findViewById(R.id.answerText);
+
+
+
+
     }
+
 }
