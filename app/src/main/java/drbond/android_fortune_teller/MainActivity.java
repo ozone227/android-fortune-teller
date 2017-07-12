b21ca66e7f54181c7bca36bf254e22d7d7ec7d7f
Ryan Bond <drbond@gmail.com>
2017-07-11 22:12:30 -0500

    initial project commit

---
+++ app/src/main/java/drbond/android_fortune_teller/MainActivity.java
@@ -0,0 +1,13 @@
+package drbond.android_fortune_teller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
